61ce0f7211b17803145c8049cc7302f65cc29c59
Ayubjon <54034733+Ayubjon@users.noreply.github.com>
2022-07-30 14:54:13 +0300

    now server&client throw exception not only print

--- Client/AbstractHelloClient.java
+++ Client/AbstractHelloClient.java
@@ -2,15 +2,16 @@ import java.util.Objects;
 
 public abstract class AbstractHelloClient implements HelloClient {
 
-    public void checkAndRun(String[] args) {
+    public void checkAndRun(String[] args) throws IllegalArgumentException, NumberFormatException {
         if (!Objects.nonNull(args) || args.length != 5) {
             System.err.println("Expected: (String)host, (int)port, (String)prefix, (int)threads, (int)requests");
-            return;
+            throw new IllegalArgumentException("Expected: (String)host, (int)port, (String)prefix, (int)threads, (int)requests");
         }
         try {
             run(args[0], Integer.parseInt(args[1]), args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]));
         } catch (NumberFormatException e) {
-            System.err.printf("Expected an integer: %s\n", e.getMessage());
+            System.err.printf("Expected an integer: %s\n", e.getLocalizedMessage());
+            throw new NumberFormatException("Expected: (String)host, (int)port, (String)prefix, (int)threads, (int)requests. But: " + e.getLocalizedMessage());
         }
     }
 }
--- Server/AbstractHelloServer.java
+++ Server/AbstractHelloServer.java
@@ -1,3 +1,4 @@
+import javax.naming.TimeLimitExceededException;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -5,15 +6,16 @@ import java.util.concurrent.TimeUnit;
 public abstract class AbstractHelloServer implements HelloServer {
     protected ExecutorService executorService;
 
-    protected void checkAndStart(String[] args) {
+    protected void checkAndStart(String[] args) throws IllegalArgumentException {
         if (!Objects.nonNull(args) || args.length != 2) {
             System.err.println("Expected: (int)port, (int)threads");
-            return;
+            throw new IllegalArgumentException("Expected: (int)port, (int)threads");
         }
         try {
             start(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         } catch (NumberFormatException e) {
             System.err.printf("Expected an integer: %s\n", e.getMessage());
+            throw new NumberFormatException("Expected: (int)port, (int)threads");
         }
     }
 
@@ -22,9 +24,11 @@ public abstract class AbstractHelloServer implements HelloServer {
         try {
             if (!executorService.awaitTermination(waitTermination, TimeUnit.SECONDS)) {
                 System.err.println("Can not close ExecutorService");
+                throw new TimeLimitExceededException("Can not close ExecutorService");
             }
         } catch (InterruptedException e) {
             System.err.printf("Interrupted when closing executors: %s\n", e.getMessage());
+            throw new InterruptedException("Interrupted when closing executors: " + e.getMessage())
         }
     }
 }
